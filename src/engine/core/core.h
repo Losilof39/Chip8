a6ba4b36939974258f39c04326f98059343ff659
Losilof39 <70483950+Losilof39@users.noreply.github.com>
2025-02-16 16:31:05 +0100

    Add engine files

---
+++ src/engine/core/core.h
@@ -0,0 +1,14 @@
+#ifndef CORE_H
+#define CORE_H
+
+#include "core/renderer/renderer2D.h"
+#include "core/renderer/font.h"
+#include "core/filesystem.h"
+#include "core/input.h"
+#include "core/log.h"
+#include "core/audio.h"
+#include "core/common.h"
+#include "zone.h"
+#include "time.h"
+
+#endif
