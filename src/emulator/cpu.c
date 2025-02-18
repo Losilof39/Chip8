6bc05618c6722d119a4a3888904ad8e8813346ea
Losilof39 <70483950+Losilof39@users.noreply.github.com>
2025-02-18 21:25:42 +0100

    added skeleton of opcode function

--- src/emulator/cpu.c
+++ src/emulator/cpu.c
@@ -73,3 +73,131 @@ void CPU_LoadProgramToMemory()
         memory[0x200 + loc] = program[loc];
     }
 }
+
+void CPU_Cycle()
+{
+
+}
+
+void CPU_ExecuteInstruction(u16 opcode)
+{
+    pc += 2;
+
+    u8 x = (opcode & 0x0F00) >> 8;
+
+    u8 y = (opcode & 0x00F0) >> 4;
+
+    switch (opcode & 0xF000) {
+        case 0x0000:
+            switch (opcode) {
+                case 0x00E0:
+                    break;
+                case 0x00EE:
+                    break;
+            }
+    
+            break;
+        case 0x1000:
+            break;
+        case 0x2000:
+            break;
+        case 0x3000:
+            break;
+        case 0x4000:
+            break;
+        case 0x5000:
+            break;
+        case 0x6000:
+            break;
+        case 0x7000:
+            break;
+        case 0x8000:
+            switch (opcode & 0xF) {
+                case 0x0:
+                    break;
+                case 0x1:
+                    break;
+                case 0x2:
+                    break;
+                case 0x3:
+                    break;
+                case 0x4:
+                    break;
+                case 0x5:
+                    break;
+                case 0x6:
+                    break;
+                case 0x7:
+                    break;
+                case 0xE:
+                    break;
+            }
+    
+            break;
+        case 0x9000:
+            break;
+        case 0xA000:
+            break;
+        case 0xB000:
+            break;
+        case 0xC000:
+            break;
+        case 0xD000:
+            break;
+        case 0xE000:
+            switch (opcode & 0xFF) {
+                case 0x9E:
+                    break;
+                case 0xA1:
+                    break;
+            }
+    
+            break;
+        case 0xF000:
+            switch (opcode & 0xFF) {
+                case 0x07:
+                    break;
+                case 0x0A:
+                    break;
+                case 0x15:
+                    break;
+                case 0x18:
+                    break;
+                case 0x1E:
+                    break;
+                case 0x29:
+                    break;
+                case 0x33:
+                    break;
+                case 0x55:
+                    break;
+                case 0x65:
+                    break;
+            }
+    
+            break;
+    
+        default:
+            log_error("Unknown opcode %04x", opcode);
+    }
+}
+
+void CPU_UpdateTimers()
+{
+    if (delayTimer > 0) {
+        delayTimer -= 1;
+    }
+
+    if (soundTimer > 0) {
+        soundTimer -= 1;
+    }
+}
+
+void CPU_PlaySound()
+{
+    if (soundTimer > 0) {
+        // play since wave at 440
+    } else {
+        // stop playing
+    }
+}
--- src/emulator/cpu.h
+++ src/emulator/cpu.h
@@ -7,5 +7,9 @@ void CPU_Init();
 void CPU_Cleanup();
 void CPU_LoadSpriteToMemory();
 void CPU_LoadProgramToMemory();
+void CPU_Cycle();
+void CPU_ExecuteInstruction(u16 opcode);
+void CPU_UpdateTimers();
+void CPU_PlaySound();
 
 #endif
